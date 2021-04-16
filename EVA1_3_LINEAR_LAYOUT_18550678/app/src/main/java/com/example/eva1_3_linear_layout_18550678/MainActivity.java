4f603fb113a4fcd1c54c4856f0b898ef96730a65
lopezh48 <56287239+lopezh48@users.noreply.github.com>
2021-04-15 21:53:14 -0600

    Add files via upload

---
+++ EVA1_3_LINEAR_LAYOUT_18550678/app/src/main/java/com/example/eva1_3_linear_layout_18550678/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.eva1_3_linear_layout_18550678;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
